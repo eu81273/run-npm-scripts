f5058779c52df3f3bdead2eaade7bafb13303793
Aiden Ahn <eu81273@gmail.com>
2017-06-02 22:58:00 +0900

    First commit :rocket:

---
+++ README.md
@@ -0,0 +1,37 @@
+run-npm-scripts is a command-line tool that allows you to run scripts listed on package.json in interactive way.
+
+![run-npm-scripts-screenshot](https://cloud.githubusercontent.com/assets/2687916/26728412/1e394278-47e5-11e7-867d-af88da821d17.gif)
+
+Installation
+--------------
+
+```sh
+npm install -g run-npm-scripts
+```
+
+Usage
+--------------
+Show npm scripts listed on package.json in current directory:
+
+```sh
+$ rns
+
+? Select npm script to run
+
+❯ start
+  build
+  test
+```
+
+
+Requirements
+--------------
+
+node.js 6.4.0+
+
+
+License
+--------------
+The MIT License.
+
+Copyright ⓒ 2017 Aiden Ahn.
---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const co = require('co');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+co(function* () {
+    const packageData = fs.readFileSync('./package.json');
+    const { scripts = {} } = JSON.parse(packageData);
+    const { target } = yield inquirer.prompt([{
+        type: 'list',
+        name: 'target',
+        message: 'Select npm script to run\n',
+        choices: Object.keys(scripts)
+    }]);
+    const [ cmd, ...args ] = scripts[target].split(' ');
+
+    spawn(cmd, args, { stdio: 'inherit' });
+})
+.catch(function () {
+    console.log(chalk.blue('package.json'), 'not found.');
+});
